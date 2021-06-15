529003c3cbc9aaa8ae5bc4101a37fbbeb2b4676a
Michael Williams <mikej803@yahoo.com>
2021-06-14 20:39:43 -0400

    created database and tables

---
+++ init.bash
@@ -0,0 +1,2 @@
+sequelize model:generate --name users --attributes fName:string,lName:string,email:string,username:string,password:string,roleID:integer
+sequelize model:generate --name roles --attributes title:string,rank:integer
---
+++ migrations/20210614202725-create-users.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fName: {
+        type: Sequelize.STRING
+      },
+      lName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      roleID: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'roles',
+          key: 'id'
+        }   
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    },
+    {freezeTableName:true});
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};

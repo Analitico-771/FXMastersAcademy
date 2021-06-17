a629096adc1c6ca39aa2380358bfe0889fe5fa26
BrandonStinson <Brandonstinson5@icloud.com>
2021-06-16 22:09:12 -0400

    content updates

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
 
-
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
@@ -8,40 +7,29 @@ const cookieSession = require('cookie-session');
 //! do this
 const passport = require('passport');
 let PORT = 3000;
-
 //public
 app.use(express.static('public'));
 app.use(helmet());
-
 //cookie-session 
 app.use(cookieSession({
     name: 'session',
     keys: ['lskdfjl;sj;lasjdfl;ajsld;fjasl;djflasjdflsak'], 
     maxAge: 14 * 24  * 60 * 60 * 1000
 }))
-
 //views
 app.set('view engine', 'ejs')
 //! do this also
 app.use(passport.initialize());
 app.use(passport.session());
-
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-
 //routes 
-
-
 app.use(require('./routes/login')) 
 app.use(require('./routes/index'))
 app.use(require('./routes/about'))
 app.use(require('./routes/teachers'))
 app.use(require('./routes/contact'))
-
 app.use(require('./routes/registration'))
-
-
-
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`);
 })
